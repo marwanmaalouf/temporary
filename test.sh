1f8c25218e698b4d68dae6b6267dab147297be1d
marwanmaalouf <marwanmaalouf1996@gmail.com>
2018-03-29 19:31:35 +0300

    Updated bash file

--- test.sh
+++ test.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
-echo '----------initial run:-------------------'
+
+echo '----------Downloading files:-------------------'
+git clone https://github.com/marwanmaalouf/temporary.git
+echo '-----------------------------------------------'
+
+echo '----------initial run:-------------------------'
 mvn -q starts:starts
-echo '-----------------------------'
+echo '-----------------------------------------------'
 
-echo '----------Test 1:-------------------'
+echo '----------Test 1:------------------------------'
 mvn -q starts:starts
-echo '------------------------------------'
+echo '-----------------------------------------------'
 
-
+echo '----------Modifying class:---------------------'
 sed -i "s/n1 + n2/n1 + n2 + 0/g" src/main/java/com/javacodegeeks/Calc.java
-echo '----------Test 2:-------------------'
-mvn -q starts:starts
-echo '------------------------------------'
+echo '-----------------------------------------------'
 
-## steps
-## 1. download a project with surefire version < 2.13
-## 2. run mvn starts:starts 
-## 3. run mvn starts:starts
-## if tests are ran in the second run than all tests are being run
-## 4. modify one class that is tested
-## 5. run mvn starts:starts
-## if only affected tests are ran than failed, if all tests are ran than passed
+echo '----------Test 2:------------------------------'
+mvn -q starts:starts
+echo '-----------------------------------------------'
